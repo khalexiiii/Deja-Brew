39d8f0114271c154d4fadbf9d6fd6c9548a0ab87
khalexiiii <alexandrite.llarenas@gmail.com>
2023-04-10 15:35:27 +0800

    Update

--- src/components/pages/Menu.jsx
+++ src/components/pages/Menu.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import '../../App.css';
+import Breadcrumb from 'react-bootstrap/Breadcrumb'
 
 export default function Menu() {
-  return <h1 className='menu'>Menu</h1>;
+  return (
+    <div className="menu">     
+      <Breadcrumb>
+        <div className="container">
+          <div class="category-page-title">Menu</div>
+          <Breadcrumb.Item href="/Deja-Brew/">Home</Breadcrumb.Item>
+          <Breadcrumb.Item active >Menu</Breadcrumb.Item>
+        </div>
+      </Breadcrumb>
+      <div className="container menu-content"></div>
+    </div>
+  )
 }
