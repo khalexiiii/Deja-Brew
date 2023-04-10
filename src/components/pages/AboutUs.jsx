52bc5717bb3f247f08fa2bae33967b01d50eaf93
khalexiiii <alexandrite.llarenas@gmail.com>
2023-04-10 14:16:48 +0800

    Update

--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import '../../App.css';
+import Breadcrumb from 'react-bootstrap/Breadcrumb'
 
 export default function AboutUs() {
-  return <h1 className='about-us'>About Us</h1>;
+  return (
+    <div className="about-us">     
+        <Breadcrumb>
+          <div className="container">
+            <Breadcrumb.Item href="/Deja-Brew/">Home</Breadcrumb.Item>
+            <Breadcrumb.Item active >About</Breadcrumb.Item>
+          </div>
+        </Breadcrumb>
+    </div>
+  )
 }
