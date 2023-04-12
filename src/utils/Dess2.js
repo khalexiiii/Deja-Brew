e56bd39861fef527e60a20544da8bb9faed68704
SimonBFornillos <simonbfornillos@gmail.com>
2023-04-12 10:42:58 +0800

    Simon Update

--- src/utils/Dess2.js
+++ src/utils/Dess2.js
@@ -2,7 +2,7 @@ const Dess2 = {
     method: 'GET',
     url: 'https://beverages-and-desserts.p.rapidapi.com/desserts',
     headers: {
-        'X-RapidAPI-Key': 'e4f0dafd24msh5d90dedefaa957ep1b78e8jsn868aa37b9c19',
+        'X-RapidAPI-Key': '105016ed5dmsh0b98d039025152ap184a51jsnec5b4eea0ee6',
         'X-RapidAPI-Host': 'beverages-and-desserts.p.rapidapi.com'
     }
 };
